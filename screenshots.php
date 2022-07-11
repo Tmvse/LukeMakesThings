e510f4d7c85758511d6970213460e0f75cad90b6
Tmvse <109001312+Tmvse@users.noreply.github.com>
2022-07-11 19:13:52 +0100

    Create screenshots.php

---
+++ screenshots.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Screenshots</title>
+</head>
+
+<html>
+<body>
+    <img src="lol.png.jpeg" width="580px" height="420px">
+</body>
+<html>
